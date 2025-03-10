c16cd605781881cd7c3e8e0b0a38b8391f2854a6
medlaq777 <m.laqsoumi@gmail.com>
2025-03-10 11:15:52 +0000

    create the controller produit file with all nessecary functions

--- app/Http/Controllers/ProduitController.php
+++ app/Http/Controllers/ProduitController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produit;
 
 class ProduitController extends Controller
 {
-    //
+    public function index(){
+        return Produit::all();
+    }
+
+    public function store(Request $request){
+        $produit = Produit::create($request->all());
+        return response()->json($produit, 201);
+    }
+
+    public function show($id){
+        return Produit::findorfail($id);
 }
